6aae9e09b7af971bb304deadbb3207fbc71057c9
machelinos <marcel.cabrera@gmail.com>
2020-01-21 20:16:26 -0600

    Projects section and styles

--- wp-content/themes/marcelcabrera/front-page.php
+++ wp-content/themes/marcelcabrera/front-page.php
@@ -52,6 +52,48 @@
     <!-- .skills -->
     <?php endif; ?>
 
+    <?php if(have_rows('proyectos')): ?>
+    <section class="section projects text-center">
+        <div class="container">
+            <div class="entry rte">
+                <h2>Mis Proyectos Recientes</h2>
+                <p>Estos son algunos de mis proyectos de diseño y desarrollo web recientes. ¿Te interesa ver más? <a href="mailto:marcel@marcelcabrera.com.mx">Envíame un correo.</a></p>
+            </div>
+            <!-- .entry -->
+
+            <div class="row">
+                <?php while(have_rows('proyectos')): the_row();
+                    $img_proyecto= get_sub_field('proyecto_imagen');
+                ?>
+                <article class="col-md-4 project">
+                    <div class="project-wrapper">
+                        <img src="<?php echo $img_proyecto['url'] ?>" alt="<?php echo $img_proyecto['alt']; ?>">
+                        
+                        <div class="project-content">
+                            <div class="content-wrapper">
+                                <h3><?php the_sub_field('proyecto_descripcion'); ?></h3>
+                                <p><a href="<?php the_sub_field('proyecto_enlace'); ?>" class="btn" target="_blank">Ver sitio web</a></p>
+                            </div>
+                            <!-- .content-wrapper -->
+
+                        </div>
+                        <!-- .project-content -->
+                    </div>
+                    <!-- .project-wrapper -->
+                
+                </article>
+                <!-- .project -->
+                <?php endwhile; ?>
+            </div>
+            <!-- .row -->
+
+        </div>
+        <!-- .container -->
+    
+    </section>
+    <!-- .projects -->
+    <?php endif; ?>
+
 <?php
     endwhile; endif;
     get_footer(); 
