370e1591a1172cf7644fe855768a95f491a177ee
machelinos <marcel.cabrera@gmail.com>
2020-01-22 19:22:40 -0600

    Footer section

--- wp-content/themes/marcelcabrera/footer.php
+++ wp-content/themes/marcelcabrera/footer.php
@@ -1,38 +1,17 @@
         </main>
         
-        <footer class="footer">
+        <footer class="footer section">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                    <?php 
+
+                <div class="social text-center">
+                <?php 
                         if(is_active_sidebar('footer_1')):
                             dynamic_sidebar('footer_1');
                         endif;
                     ?>
-                    </div>
-                    <!-- .col-md-4 -->
-
-                    <div class="col-md-4">
-                    <?php 
-                        if(is_active_sidebar('footer_2')):
-                            dynamic_sidebar('footer_2');
-                        endif;
-                    ?>
-                    </div>
-                    <!-- .col-md-4 -->
-
-                    <div class="col-md-4">
-                    <?php 
-                        if(is_active_sidebar('footer_3')):
-                            dynamic_sidebar('footer_3');
-                        endif;
-                    ?>
-                    </div>
-                    <!-- .col-md-4 -->
-
-
+                
                 </div>
-                <!-- .row -->
+                <!-- social -->
 
                 <div>
                     <p class="text-center"><?php _e('Todos los derechos reservados &copy;', 'wpboilerplate'); bloginfo('name'); echo ' '.date('Y'); ?></p>
